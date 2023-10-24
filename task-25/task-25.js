0da1c74b689af4d39f4f8f59289f3914c858fd2e
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-10-25 00:36:08 +0300

    Update task-25.js

--- task-25/task-25.js
+++ task-25/task-25.js
@@ -1,9 +1,16 @@
-function createElementStyle(tagName,text,styles) {
+/*25.	Задача: Создать и добавить стиль для элемента: Напишите функцию, которая создает новый элемент,
+ добавляет его в DOM и устанавливает для него стиль с помощью CSS.
+ */
+function createElementStyle(tagName, text, styles) {
+   //создаем новый элемент с указанным тегом
    const newElement = document.createElement(tagName);
+   //устанавливаем текстовое содержимое
    newElement.textContent = text;
+   //проходимся по всем ключам объекта и устанавливаем стили по ключу
    for (let key in styles) {
       newElement.style[key] = styles[key];
    }
+   //добавляем созданный элемент в конец тела body
    document.body.appendChild(newElement);
 }
 const stylesBlock = {
