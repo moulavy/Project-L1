fb0c213c11cfe74825e3c3ea8862fc9f1e4bb6d4
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-10-26 16:26:16 +0300

    22

---
+++ task-22/task-22.js
@@ -0,0 +1,14 @@
+let max = 0; // Переменная для отслеживания максимального количества вызовов document.write()
+
+function recursiveDocumentWrite() {
+   try {
+      max++; // Увеличиваем счетчик каждый раз, когда вызывается document.write()
+      document.write('<div>Текст</div>'); // Пишем текст в документ
+      recursiveDocumentWrite(); // Рекурсивный вызов функции
+   } catch (error) {
+      console.log('Максимальное количество раз:', max); 
+      console.error('Произошла ошибка:', error); 
+   }
+}
+
+recursiveDocumentWrite(); 
